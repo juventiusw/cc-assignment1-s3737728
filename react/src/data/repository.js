ef29523fbe4bff244793debc41379ac16f3cbe2f
Juventius Wu <s3737728@student.rmit.edu.au>
2022-04-12 02:06:43 +1000

    update

--- react/src/data/repository.js
+++ react/src/data/repository.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 // --- Constants ----------------------------------------------------------------------------------
-//const localhost = "http://localhost:4000";
-const cloudfront = "https://d19w8d22e3i0ra.cloudfront.net";
-const API_HOST = cloudfront; // only set to localhost when Express and React are running in local machine.
+const API_HOST = "http://localhost:4000";
 
 async function getData() {
     const response = await axios.get(API_HOST + "/api");
