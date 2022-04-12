e5fc7388081fdd0696c4e7d699158c53b7932f55
Juventius Wu <s3737728@student.rmit.edu.au>
2022-04-13 01:27:00 +1000

    update

--- react/src/data/repository.js
+++ react/src/data/repository.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // --- Constants ----------------------------------------------------------------------------------
 const localhost = "http://localhost:4000";
-const cloudfront = "https://d3k0blzet9rzk5.cloudfront.net";
+// const cloudfront = "https://d3k0blzet9rzk5.cloudfront.net";
 const API_HOST = localhost; // assign cloudfront to API_HOST and delete localhost constant when deploying to s3
 
 async function getData() {
